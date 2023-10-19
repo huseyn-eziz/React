b172de49ded6d96d3821d6c7dd8ff6025943c8de
huseyn-eziz <mastereziz@gmail.com>
2023-10-19 22:00:00 +0400

    n

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const url="...//"
+  const [ users, setUsers ] = useState([])
+
+useEffect(() => { 
+  axios.get(url).then(({data})=> {
+    setUsers(data)
+  })
+ },[]);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {users.map(({id,name,surname})=>{
+          return (
+            <div key={id}>
+            {name}
+            {surname}
+            </div>
+            );
+        }
+      )}
     </>
   )
 }
