a1f013e80efc0b9d3ae7518eb0d13bf13f9ad52d
huseyn-eziz <mastereziz@gmail.com>
2023-10-26 10:07:24 +0400

    create basic proje picture

---
+++ Basic-projects/picture-app/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <body>
+
+      <div className='picture'>
+        <div className="pictureesas">
+          <img src="./images/qarisiq.jpeg" alt="" />
+        </div>
+        <div className="pictures">
+          <img src="./images/qarisiq.jpeg" alt="" />
+          <img src="./images/lule.jpeg" alt="" />
+          <img src="./images/tike.jpeg" alt="" />
+          <img src="./images/toyuq.jpeg" alt="" />
+        </div>
+      </div>
+    </body>
+  )
+}
+
+export default App
