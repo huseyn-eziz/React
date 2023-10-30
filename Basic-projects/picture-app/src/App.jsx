46029550cc69f82338a3c76fbf404cb8d5cd2ddc
huseyn-eziz <mastereziz@gmail.com>
2023-10-30 15:21:47 +0400

    picture-app finished

--- Basic-projects/picture-app/src/App.jsx
+++ Basic-projects/picture-app/src/App.jsx
@@ -1,19 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+const [ click, setClick ] = useState("./images/qarisiq.jpeg")
+
+const srcs = [ "./images/qarisiq.jpeg",
+"./images/lule.jpeg",
+"./images/tike.jpeg",
+"./images/toyuq.jpeg"]
+
 
   return (
     <body>
-
-      <div className='picture'>
-        <div className="pictureesas">
-          <img src="./images/qarisiq.jpeg" alt="" />
+      <div className='img'>
+        <div className="imgesas">
+        <img src={click} alt="" />
         </div>
-        <div className="pictures">
-          <img src="./images/qarisiq.jpeg" alt="" />
-          <img src="./images/lule.jpeg" alt="" />
-          <img src="./images/tike.jpeg" alt="" />
-          <img src="./images/toyuq.jpeg" alt="" />
+        <div className="imgs">
+        { srcs.map((click,index) => 
+          <img onClick={()=> setClick(click)} key={index} src={click} alt="" />
+        )}
+
         </div>
       </div>
     </body>
