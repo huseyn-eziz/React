fe414741d483356b3b741a53844cfed51a1b26a3
huseyn-eziz <mastereziz@gmail.com>
2023-10-30 17:45:32 +0400

    todo finish

--- Basic-projects/todo-app/src/App.jsx
+++ Basic-projects/todo-app/src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState('')
+  const [todos, setTodos] = useState([])
 
   return (
     <>
-    a
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={() => todo.trim() !== '' && setTodos([...todos, todo], setTodo(''))}> ButtonAdd </button>
+
+      {todos.map((todolar) => {
+        return (
+
+          <ul>
+            <li> {todolar} </li>
+          </ul>
+
+        )
+
+      })
+      }
     </>
   )
 }
