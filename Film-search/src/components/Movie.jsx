ffeafc35d6afe70def8a89521651538fb66baa1e
huseyn-eziz <mastereziz@gmail.com>
2023-11-09 15:21:02 +0400

    movie finish

--- Film-search/src/components/Movie.jsx
+++ Film-search/src/components/Movie.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Movie = ({ Title, Year, Poster }) => {
     return (
-        <div className='my-8 group relative rounded-xl overflow-hidden hover:scale-105 hover:shadow-2xl hover:shadow-[rgba(255,255,255,0.5)] transition-all'>
+        <div className='my-8 group relative rounded-xl overflow-hidden hover:scale-105 transition-all duration-100 ease-in hover:shadow-2xl hover:shadow-[rgba(255,255,255,0.5)] '>
 
             <img src={Poster == "N/A" ? "https://fakeimg.pl/300x433?text=Poster&font=museo" : Poster}
                 alt={Title}
             />
-            <div className=' bg-gradient-to-r from-zinc-950 to-transparent py-4 px-2 absolute bottom-[-100%] group-hover:bottom-0 left-0 w-full transition-all  '>
+            <div className=' bg-gradient-to-r from-zinc-950 to-transparent py-4 px-2 absolute bottom-[-100%] group-hover:bottom-0 left-0 w-full transition-all ease-linear duration-100'>
                 <h2 className='text-white text-lg font-semibold'>{Title}</h2>
                 <p className='text-white text-lg font-thin'>Year: {Year}</p>
             </div>
